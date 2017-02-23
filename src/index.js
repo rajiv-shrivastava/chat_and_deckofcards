7c4df5a48997e7c361cac99104929ccf04601a7e
rajiv-shrivastava <rjv834@gmail.com>
2017-02-23 12:01:29 +0530

    removed extra files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
+import ReactDOM from 'react-dom'
+
 import Cards from './components/Cards'
-import Spades from './components/Spades'
 
-import reducer from './reducers'
 
-const store = createStore(reducer)
 
-render(
-  <Provider store={store}>
-    <Cards />
-  </Provider>,
-  document.getElementById('root')
-  )
+ReactDOM.render(<Cards />,  document.getElementById('root')
+)
